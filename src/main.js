2d71d74bf823b30643ccaeacba8e9e54f1a46662
Juryii <polyanskii2118@gmail.com>
2020-02-07 00:08:19 +0600

    add validation register page and add message plugin

--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ import store from './store'
 import dateFilter from "./filters/date.filter";
 import 'materialize-css/dist/js/materialize.min'
 import Vuelidate from "vuelidate/src";
-
+import messagePlugin from './utils/message.plugin'
 Vue.config.productionTip = false;
 
+Vue.use(messagePlugin);
 Vue.filter('date', dateFilter);
-
 Vue.use(Vuelidate);
 
 new Vue({
